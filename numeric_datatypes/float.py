575dc0f3e5d61c6a81ce1cf448232a2de52b9086
Aravind <aravindmunna240@gmail.com>
2024-10-05 18:09:48 +0530

    float and int numeric datatype examples

---
+++ numeric_datatypes/float.py
@@ -0,0 +1,10 @@
+num1 = 5.0
+num2 = 2.5
+
+# Basic addition
+sum = num1 + num2
+print("Additon result ",sum)
+
+# Rounding
+round_result = round(3.14159265359,2)  # Rounds to 2 places
+print("Rounded : ",round_result)
---
+++ numeric_datatypes/int.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+
+#Division
+division_result = num1 // num2
+print("Integer Division ",division_result)
+
+#Mod
+mod_result = num1 % num2
+print("Modulus (remainder): ",mod_result)
+
+#Abs value
+abs_result = abs(-7)
+print("Absolute value ",abs_result)
