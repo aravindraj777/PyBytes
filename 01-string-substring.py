f88f2bf8f1f04d5465240e681c91579a4d55e0c7
Aravind <aravindmunna240@gmail.com>
2024-10-05 17:50:39 +0530

    strings and String functions in python

---
+++ 01-string-substring.py
@@ -0,0 +1,4 @@
+text = "Python is awesome"
+subtring = "is"
+if subtring in text:
+    print(subtring, " found in the text")
---
+++ string-concat.py
@@ -0,0 +1,4 @@
+str1 = "Hello"
+str2 = "Hai"
+result = str1 + " "+str2
+print("This is the result "+ result)
