b0440ff00394a2302b3588a298892597d1b10ac7
Aravind <aravindmunna240@gmail.com>
2024-10-06 17:28:18 +0530

    list and list operations

---
+++ Day-06/lists.py
@@ -0,0 +1,22 @@
+
+list = ["sreekanth_demo_s3","Abhijith_demo_s3","Shyam_demo_s3"]
+print(list)
+
+# length
+print(len(list))
+
+# adding and removing
+list.append("Aravind_demo_s3")
+list.remove("Abhijith_demo_s3")
+
+print(list)
+print(len(list))
+
+# slicing lists
+new_list = list[0:2]
+print(new_list)
+
+# sorting
+number_list = [1,23,12,0,34,32]
+number_list.sort()
+print(number_list)
