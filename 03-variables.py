f7683e7bb5126240541ff0891a3d4b2495140f4c
Aravind <aravindmunna240@gmail.com>
2024-10-06 14:39:41 +0530

    command line args

--- 03-variables.py
+++ 03-variables.py
@@ -1,10 +1,21 @@
+import sys
+
 a = 10
 b = 20
-def addition():
-    print(a + b)
+def addition(num1, num2):
+    print("Method calling")
+    return num1 + num2
 
 def substraction():
     print(a - b)
 
-addition()
+# addition()
 substraction()
+
+num1 = float(sys.argv[1])
+operation = sys.argv[2]
+num2 = float(sys.argv[3])
+
+if operation == "addition":
+    output = addition(num1,num2)
+    print("command line outout ",output)
