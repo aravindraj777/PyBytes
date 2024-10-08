414df51e179a78fc8419db612c7af611ce68dfb4
Aravind <aravindmunna240@gmail.com>
2024-10-08 23:46:36 +0530

    Python oops classes with object and property accessing

---
+++ Day-08/oops/car.py
@@ -0,0 +1,14 @@
+class Car:
+    
+    def __init__(self, model, year, color, for_sale):
+        self.model = model
+        self.year = year
+        self.color = color
+        self.for_sale = for_sale
+
+
+car1 = Car("Mustang",2024,"red",False)
+car2 = Car("Corvete", 2025, "blue",True)
+
+print(car1.model)        
+        
